ac3e52398976b5e98aacbc18d76ee3dd53d0dd56
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-22 16:21:10 +0200

    Worked on test suite

---
+++ test/fixtures/plain.js
@@ -0,0 +1,2 @@
+import index from "./script"
+export default index
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import fs from "fs"
 import rimraf from "rimraf"
-import rollup from "rollup"
+import { rollup } from "rollup"
+import denodeify from "denodeify"
 import test from "ava"
 
 import relinkPlugin from "../src"
@@ -8,20 +9,29 @@ import relinkPlugin from "../src"
 const dest = "output/output.js"
 const outputFolder = "./src"
 
+const stat = denodeify(fs.stat)
+
 process.chdir(__dirname)
 
-function run(entry, limit) {
-  return rollup.rollup({
+function run(entry) 
+{
+  return rollup({
     entry,
-    plugins: [relinkPlugin({ outputFolder })],
-  }).then(bundle => bundle.write({
-    dest,
-  })).then(() => plugin.write({dest}))
+    plugins: [ relinkPlugin({ outputFolder }) ]
+  })
+  .then(bundle => bundle.write({
+    dest
+  }))
 }
 
-function assertExists(name, shouldExist = true) {
-  return promise(fs.stat, name)
-  .then(stat => true, err => false)
-  .then(exists => assert.ok(exists === shouldExist))
+function fileExists(name) 
+{
+  return stat(name)
+    .then(stat => true, err => false)
 }
 
+test(t => {
+  return run("./fixtures/plain.js").then(() => Promise.all([
+    fileExists("./output/output.js").then((exists) => t.true(exists))
+  ]))
+})
