442d301c6134cf1594640903dbd2d2c3e6a9d53c
Sebastian Werner <s.werner@sebastian-software.de>
2016-10-18 17:42:03 +0200

    Added deep reference to asset from JS (which requires fix)

--- test/fixtures/assets.js
+++ test/fixtures/assets.js
@@ -1,4 +1,5 @@
 import font from "./font.woff"
 import image from "./image.png"
+import deep from "./deep/folder/blank.gif"
 
-export default font + "|" + image
+export default font + "|" + image + "|" + deep
