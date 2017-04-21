619532a81912d2d342ce2fb7ec89a214a72d2f2f
Sebastian Werner <s.werner@sebastian-software.de>
2017-04-21 16:05:14 +0200

    Linting fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,1 @@
-/* eslint-disable import/no-commonjs */
 require("readable-code")
--- test/index.js
+++ test/index.js
@@ -52,8 +52,9 @@ test((check) => {
   return bundle("./fixtures/assets.js", outputFile).then(() => Promise.all([
     fileExists(outputFile).then((exists) => check.true(exists)),
     readFile(outputFile, "utf-8").then((content) => {
+      /* eslint-disable no-template-curly-in-string */
       var expectedContent = "import _VrPi9W49 from './font-VrPi9W49.woff';\nimport _l1JhGTH9 from './image-l1JhGTH9.png';\nimport _hk4Yl7Ly from './blank-hk4Yl7Ly.gif';\n\nvar assets = `${_VrPi9W49}|${_l1JhGTH9}|${_hk4Yl7Ly}`;\n\nexport default assets;\n"
-      check.is(content, expectedContent)
+      return check.is(content, expectedContent)
     }),
     fileExists(imageFile).then((exists) => check.true(exists)),
     fileExists(fontFile).then((exists) => check.true(exists)),
@@ -74,8 +75,9 @@ test((check) => {
   return bundle("./fixtures/deep/assets-outside.js", outputFile).then(() => Promise.all([
     fileExists(outputFile).then((exists) => check.true(exists)),
     readFile(outputFile, "utf-8").then((content) => {
+      /* eslint-disable no-template-curly-in-string */
       var expectedContent = "import _VrPi9W49 from './font-VrPi9W49.woff';\nimport _l1JhGTH9 from './image-l1JhGTH9.png';\nimport _hk4Yl7Ly from './blank-hk4Yl7Ly.gif';\n\n/* eslint-disable filenames/match-regex */\nvar assetsOutside = `${_VrPi9W49}|${_l1JhGTH9}|${_hk4Yl7Ly}`;\n\nexport default assetsOutside;\n"
-      check.is(content, expectedContent)
+      return check.is(content, expectedContent)
     }),
     fileExists(imageFile).then((exists) => check.true(exists)),
     fileExists(fontFile).then((exists) => check.true(exists)),
@@ -96,8 +98,9 @@ test((check) => {
   return bundle("./fixtures/deep/assets-mixed.js", outputFile).then(() => Promise.all([
     fileExists(outputFile).then((exists) => check.true(exists)),
     readFile(outputFile, "utf-8").then((content) => {
+      /* eslint-disable no-template-curly-in-string */
       var expectedContent = "import _VrPi9W49 from './font-VrPi9W49.woff';\nimport _YauiPPOt from './cappuccino-YauiPPOt.svg';\nimport _hk4Yl7Ly from './blank-hk4Yl7Ly.gif';\n\n/* eslint-disable filenames/match-regex */\nvar assetsMixed = `${_VrPi9W49}|${_YauiPPOt}|${_hk4Yl7Ly}`;\n\nexport default assetsMixed;\n"
-      check.is(content, expectedContent)
+      return check.is(content, expectedContent)
     }),
     fileExists(fontFile).then((exists) => check.true(exists)),
     fileExists(svgFile).then((exists) => check.true(exists)),
