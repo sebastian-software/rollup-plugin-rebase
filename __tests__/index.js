dc8e08ccd3bc187bfe970f567573b74bb8b5569f
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-20 11:34:52 +0200

    Rewrote test suite using jest

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,7 +1,6 @@
 import fs from "fs-extra"
 import { rollup } from "rollup"
 import denodeify from "denodeify"
-import test from "ava"
 import shortid from "shortid"
 
 import rebasePlugin from "../src"
@@ -35,30 +34,32 @@ function fileExists(name)
     .then((result) => true, (error) => false)
 }
 
-test((check) => {
+test("Plain", () => {
   var outputFile = `${outputFolder}${shortid()}.js`
   return bundle("./fixtures/plain.js", outputFile).then(() => Promise.all([
-    fileExists(outputFile).then((exists) => check.true(exists)),
+    expect(fileExists(outputFile)).resolves.toBeTruthy()
+  ])).then(Promise.all([
     rimraf(outputFile)
   ]))
 })
 
-test((check) => {
+test("Assets", () => {
   var outputFile = `${outputFolder}${shortid()}.js`
   var imageFile = `${outputFolder}image-l1JhGTH9.png`
   var fontFile = `${outputFolder}font-VrPi9W49.woff`
   var deepFile = `${outputFolder}blank-hk4Yl7Ly.gif`
 
   return bundle("./fixtures/assets.js", outputFile).then(() => Promise.all([
-    fileExists(outputFile).then((exists) => check.true(exists)),
+    expect(fileExists(outputFile)).resolves.toBeTruthy(),
     readFile(outputFile, "utf-8").then((content) => {
       /* eslint-disable no-template-curly-in-string */
       var expectedContent = "import _VrPi9W49 from './font-VrPi9W49.woff';\nimport _l1JhGTH9 from './image-l1JhGTH9.png';\nimport _hk4Yl7Ly from './blank-hk4Yl7Ly.gif';\n\nvar assets = `${_VrPi9W49}|${_l1JhGTH9}|${_hk4Yl7Ly}`;\n\nexport default assets;\n"
-      return check.is(content, expectedContent)
+      expect(content).toBe(expectedContent)
     }),
-    fileExists(imageFile).then((exists) => check.true(exists)),
-    fileExists(fontFile).then((exists) => check.true(exists)),
-    fileExists(deepFile).then((exists) => check.true(exists)),
+    expect(fileExists(imageFile)).resolves.toBeTruthy(),
+    expect(fileExists(fontFile)).resolves.toBeTruthy(),
+    expect(fileExists(deepFile)).resolves.toBeTruthy()
+  ])).then(Promise.all([
     rimraf(outputFile),
     rimraf(imageFile),
     rimraf(fontFile),
@@ -66,22 +67,23 @@ test((check) => {
   ]))
 })
 
-test((check) => {
+test("Outside Assets", () => {
   var outputFile = `${outputFolder}${shortid()}.js`
   var imageFile = `${outputFolder}image-l1JhGTH9.png`
   var fontFile = `${outputFolder}font-VrPi9W49.woff`
   var deepFile = `${outputFolder}blank-hk4Yl7Ly.gif`
 
   return bundle("./fixtures/deep/assets-outside.js", outputFile).then(() => Promise.all([
-    fileExists(outputFile).then((exists) => check.true(exists)),
+    expect(fileExists(outputFile)).resolves.toBeTruthy(),
     readFile(outputFile, "utf-8").then((content) => {
       /* eslint-disable no-template-curly-in-string */
       var expectedContent = "import _VrPi9W49 from './font-VrPi9W49.woff';\nimport _l1JhGTH9 from './image-l1JhGTH9.png';\nimport _hk4Yl7Ly from './blank-hk4Yl7Ly.gif';\n\n/* eslint-disable filenames/match-regex */\nvar assetsOutside = `${_VrPi9W49}|${_l1JhGTH9}|${_hk4Yl7Ly}`;\n\nexport default assetsOutside;\n"
-      return check.is(content, expectedContent)
+      expect(content).toBe(expectedContent)
     }),
-    fileExists(imageFile).then((exists) => check.true(exists)),
-    fileExists(fontFile).then((exists) => check.true(exists)),
-    fileExists(deepFile).then((exists) => check.true(exists)),
+    expect(fileExists(imageFile)).resolves.toBeTruthy(),
+    expect(fileExists(fontFile)).resolves.toBeTruthy(),
+    expect(fileExists(deepFile)).resolves.toBeTruthy()
+  ])).then(Promise.all([
     rimraf(outputFile),
     rimraf(imageFile),
     rimraf(fontFile),
@@ -89,22 +91,23 @@ test((check) => {
   ]))
 })
 
-test((check) => {
+test("Mixed Assets", () => {
   var outputFile = `${outputFolder}${shortid()}.js`
   var fontFile = `${outputFolder}font-VrPi9W49.woff`
   var svgFile = `${outputFolder}cappuccino-YauiPPOt.svg`
   var deepFile = `${outputFolder}blank-hk4Yl7Ly.gif`
 
   return bundle("./fixtures/deep/assets-mixed.js", outputFile).then(() => Promise.all([
-    fileExists(outputFile).then((exists) => check.true(exists)),
+    expect(fileExists(outputFile)).resolves.toBeTruthy(),
     readFile(outputFile, "utf-8").then((content) => {
       /* eslint-disable no-template-curly-in-string */
       var expectedContent = "import _VrPi9W49 from './font-VrPi9W49.woff';\nimport _YauiPPOt from './cappuccino-YauiPPOt.svg';\nimport _hk4Yl7Ly from './blank-hk4Yl7Ly.gif';\n\n/* eslint-disable filenames/match-regex */\nvar assetsMixed = `${_VrPi9W49}|${_YauiPPOt}|${_hk4Yl7Ly}`;\n\nexport default assetsMixed;\n"
-      return check.is(content, expectedContent)
+      expect(content).toBe(expectedContent)
     }),
-    fileExists(fontFile).then((exists) => check.true(exists)),
-    fileExists(svgFile).then((exists) => check.true(exists)),
-    fileExists(deepFile).then((exists) => check.true(exists)),
+    expect(fileExists(fontFile)).resolves.toBeTruthy(),
+    expect(fileExists(svgFile)).resolves.toBeTruthy(),
+    expect(fileExists(deepFile)).resolves.toBeTruthy()
+  ])).then(Promise.all([
     rimraf(outputFile),
     rimraf(fontFile),
     rimraf(svgFile),
