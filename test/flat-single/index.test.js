25142b87c414611ed84d4c0549baa2d99fb099ef
Sebastian Werner <s.werner@sebastian-software.de>
2018-12-17 13:17:01 +0100

    Cleanup

--- test/flat-single/index.test.js
+++ test/flat-single/index.test.js
@@ -3,7 +3,7 @@ import { bundle, clean, list } from "../util"
 
 const root = __dirname
 
-test("Flat", async () => {
+test("Flat Single", async () => {
   await bundle(root, "index.js", "output/index.js")
 
   expect(list(root, "output")).resolves.toMatchSnapshot()
