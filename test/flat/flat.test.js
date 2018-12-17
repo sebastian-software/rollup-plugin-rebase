0fde0780b2d4f1eef496c58530ab0ede2215ca31
Sebastian Werner <s.werner@sebastian-software.de>
2018-12-17 12:55:56 +0100

    Cleanup

--- test/flat/flat.test.js
+++ test/flat/flat.test.js
@@ -4,15 +4,13 @@ import { bundle, clean, exists } from "../util"
 const root = __dirname
 
 test("Flat", async () => {
-  const output = "output/index.js"
-
-  await bundle(root, "flat.js", output)
+  await bundle(root, "flat.js", "output/index.js")
 
   expect(exists(root,
     [
-      output,
+      "output/index.js",
       "output/bsdudxaF.md"
     ])).resolves.toBeTruthy()
 
-  await clean(root, output)
+  await clean(root, "output")
 })
