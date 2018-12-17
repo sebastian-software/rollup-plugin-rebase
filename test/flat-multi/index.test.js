742057f40d5bcec9c301c7c50de8edd3dc0b9eb2
Sebastian Werner <s.werner@sebastian-software.de>
2018-12-17 13:16:00 +0100

    Added multi file flat

---
+++ test/flat-multi/index.test.js
@@ -0,0 +1,12 @@
+/* global __dirname */
+import { bundle, clean, list } from "../util"
+
+const root = __dirname
+
+test("Flat", async () => {
+  await bundle(root, "index.js", "output/index.js")
+
+  expect(list(root, "output")).resolves.toMatchSnapshot()
+
+  await clean(root, "output")
+})
